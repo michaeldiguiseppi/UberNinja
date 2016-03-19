5e6b65b158225d91a5d6e92f1ba611aa876572d9
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-19 17:18:03 -0600

    Finished moving ground, adding hero class.

--- UberNinja/GameScene.swift
+++ UberNinja/GameScene.swift
@@ -9,19 +9,23 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var movingGround: MDMovingGround!
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         backgroundColor = UIColor(red: 159.0/255.0, green: 203.0/255.0, blue: 244.0/255.0, alpha: 1.0)
         
-        let ground = SKSpriteNode(color: UIColor.brownColor(), size: CGSizeMake(view.frame.size.width, 20))
-        ground.position = view.center
-        addChild(ground)
+        movingGround = MDMovingGround(size: CGSizeMake(view.frame.size.width, 20))
+        movingGround.position = CGPointMake(0, view.frame.size.height/2)
+        addChild(movingGround)
         
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
+        movingGround.start()
     }
    
     override func update(currentTime: CFTimeInterval) {
---
+++ UberNinja/MDHero.swift
@@ -0,0 +1,14 @@
+//
+//  MDHero.swift
+//  UberNinja
+//
+//  Created by Mike DiGuiseppi on 3/19/16.
+//  Copyright © 2016 Mike DiGuiseppi. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MDHero: SKSpriteNode {
+    
+}
